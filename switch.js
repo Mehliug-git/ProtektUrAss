40c33a7532439b1bfbb0aa8b7d0495d39b60b301
bapetisteuh <66559240+bapetisteuh@users.noreply.github.com>
2024-03-21 11:59:53 +0100

    Add files via upload

--- switch.js
+++ switch.js
@@ -1,47 +1,11 @@
-const animate = gsap.timeline({ paused: false });
-    const animateBackground = new TimelineMax({ paused: false });
-    let toggle = false;
+document.getElementById("switch").addEventListener("click", changeText);
 
-    animateBackground
-        .to("body", 0.1, { backgroundImage: "none", backgroundColor: "#111" }, 0.2)
-        .set(".switch", { boxShadow: "0 0 10px rgba(255, 255, 255, 0.2)" })
-        .to(".text p", 0.1, { color: "#FFF" }, 0.2);
-
-    animate
-        .to(".toggle-button", 0.2, { scale: 0.7 }, 0)
-        .set(".toggle", { backgroundColor: "#FFF" })
-        .set(".circle", { display: "none" })
-        .to(".moon-mask", 0.2, { translateY: 20, translateX: -10 }, 0.2)
-        .to(".toggle-button", 0.2, { translateY: 49 }, 0.2)
-        .to(".toggle-button", 0.2, { scale: 0.9 })
-
-
-    document.getElementsByClassName("switch")[0].addEventListener("click", () => {
-        if(toggle){
-            animate.restart();
-            animateBackground.restart();
-
-        } else {
-            animate.reverse();
-            animateBackground.reverse();
-        }
-        toggle = !toggle;
-    });
-
-
-    chrome.storage.sync.get('etat_switch', function(data) {
-        var isSwitchOn = data.etat_switch || false;
-      
-        // Applique l'animation en fonction de l'état actuel du switch
-        if (isSwitchOn) {
-            //pour checked restart pour le faire aller de on a off et reverse pour linverse
-
-            animate.reverse();
-            animateBackground.reverse();
-        }
-      });
-
-
-
-
-      
+function changeText() {
+    var checkbox = document.getElementById("switch");
+    var comment = document.querySelector(".comment");
+    if (checkbox.checked) {
+        comment.textContent = "ANONYME";
+    } else {
+        comment.textContent = "MY ASS IS OPEN";
+    }
+}
